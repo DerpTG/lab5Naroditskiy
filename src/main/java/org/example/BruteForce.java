a8fa037f3af5fd09cb7812cc682088e9bf4112b9
DerpTG <fnaroditskiy@gmail.com>
2024-03-17 00:56:32 -0400

    brute force class

--- src/main/java/org/example/BruteForce.java
+++ src/main/java/org/example/BruteForce.java
@@ -1,4 +1,23 @@
 package org.example;
 
 public class BruteForce {
+    public void bruteForceDecrypt(String encryptedText) {
+        for (int shift = 0; shift < 26; shift++) {
+            StringBuilder decryptedText = new StringBuilder();
+
+            for (char character : encryptedText.toCharArray()) {
+                if (character != ' ' && character != '!' && character != '.' && character != ',') {
+                    int originalPosition = character >= 'a' && character <= 'z' ? character - 'a' : character - 'A';
+                    int newPosition = (originalPosition - shift) % 26;
+                    newPosition = newPosition < 0 ? newPosition + 26 : newPosition;
+                    char newCharacter = (char) ((character >= 'a' && character <= 'z') ? 'a' + newPosition : 'A' + newPosition);
+                    decryptedText.append(newCharacter);
+                } else {
+                    decryptedText.append(character);
+                }
+            }
+
+            System.out.println(shift + ": " + decryptedText);
+        }
+    }
 }
